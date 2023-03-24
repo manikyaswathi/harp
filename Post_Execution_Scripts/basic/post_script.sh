6273f2dacd1719c8b1b8a98dade32a9e52e311e7
manikyaswathi <38359678+manikyaswathi@users.noreply.github.com>
2023-03-24 13:27:43 -0400

    Update post_script.sh

--- Post_Execution_Scripts/basic/post_script.sh
+++ Post_Execution_Scripts/basic/post_script.sh
@@ -4,7 +4,7 @@ scapper_file=DataScrapper.py
 
 pwd
 
-cp /fs/scratch/PAS0536/Swathi/InstallerTest/harp/cheetah/examples/01-eulers_number/$scapper_file $scapper_file
+cp /<downlaod-dir>/harp/examples/01-eulers_number/$scapper_file $scapper_file
 
 python $scapper_file "train"
 
