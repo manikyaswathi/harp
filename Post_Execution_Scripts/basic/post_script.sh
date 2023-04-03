fad0da6b96051fb6b4856699202889418492e1f6
Carlos Guzman <42706936+guzman109@users.noreply.github.com>
2023-04-03 08:59:55 -0400

    Update post_script.sh

--- Post_Execution_Scripts/basic/post_script.sh
+++ Post_Execution_Scripts/basic/post_script.sh
@@ -4,7 +4,7 @@ scapper_file=DataScrapper.py
 
 pwd
 
-cp /<downlaod-dir>/harp/examples/01-eulers_number/$scapper_file $scapper_file
+cp /<download_dir>/harp/examples/01-eulers_number/$scapper_file $scapper_file
 
 python $scapper_file "train"
 
